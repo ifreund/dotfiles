4afaeeb1c4c25b01bf95d87d9493aa7259cffdd8
ifreund <isaac.a.freund@gmail.com>
2019-10-25 16:41:05 +0200

    Add rofi config and tweak i3

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@
 # symlink configs
 ln -s ~/git-repos/dotfiles/i3/config ~/.config/i3/config
 ln -s ~/git-repos/dotfiles/i3blocks/config ~/.config/i3blocks/config
+ln -s ~/git-repos/dotfiles/rofi/config ~/.config/rofi/config
+ln -s ~/git-repos/dotfiles/compton.conf ~/.config/compton.conf
