e72309bf1348e2850dfe85237a95acb2382d8fb7
Isaac Freund <isaac.a.freund@gmail.com>
2019-11-15 00:32:16 +0100

    Fix symlink

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ ln -s ~/git-repos/dotfiles/rofi/config ~/.config/rofi/config
 ln -s ~/git-repos/dotfiles/compton.conf ~/.config/compton.conf
 ln -s ~/git-repos/dotfiles/bashrc ~/.bashrc
 ln -s ~/git-repos/dotfiles/xprofile ~/.xprofile
-ln -s ~/git-repos/dotfiles/spicetify/Themes/ ~/.config/spicetify/Themes
+ln -s ~/git-repos/dotfiles/spicetify/Themes/ ~/.config/spicetify/
 ln -s ~/git-repos/dotfiles/zathura/zathurarc ~/.config/zathura/zathurarc
 ln -s ~/git-repos/dotfiles/xinitrc ~/.xinitrc
 ln -s ~/git-repos/dotfiles/bash_profile ~/.bash_profile
