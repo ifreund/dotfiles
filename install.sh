be3ba055728802a01146a2dd9a8ffd675a1bd181
Isaac Freund <isaac.a.freund@gmail.com>
2019-11-23 12:16:34 +0100

    Formatting cleanup and track flameshot conf

--- install.sh
+++ install.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-# symlink configs
+# standard config dir stuff
 ln -s ~/git-repos/dotfiles/config/alacritty/ ~/.config/
-ln -s ~/git-repos/dotfiles/config/i3/ ~/.config/
-ln -s ~/git-repos/dotfiles/config/i3blocks/ ~/.config/
-ln -s ~/git-repos/dotfiles/config/nnn ~/.config/
-ln -s ~/git-repos/dotfiles/config/nvim/ ~/.config/
-ln -s ~/git-repos/dotfiles/config/picom/ ~/.config/
-ln -s ~/git-repos/dotfiles/config/rofi/ ~/.config/
-ln -s ~/git-repos/dotfiles/config/zathura/ ~/.config/
+ln -s ~/git-repos/dotfiles/config/Dharkael/  ~/.config/
+ln -s ~/git-repos/dotfiles/config/i3/        ~/.config/
+ln -s ~/git-repos/dotfiles/config/i3blocks/  ~/.config/
+ln -s ~/git-repos/dotfiles/config/nnn/       ~/.config/
+ln -s ~/git-repos/dotfiles/config/nvim/      ~/.config/
+ln -s ~/git-repos/dotfiles/config/picom/     ~/.config/
+ln -s ~/git-repos/dotfiles/config/rofi/      ~/.config/
+ln -s ~/git-repos/dotfiles/config/zathura/   ~/.config/
 
-ln -s ~/git-repos/dotfiles/spicetify/Themes/ ~/.config/spicetify/
-ln -s ~/git-repos/dotfiles/Xresources ~/.Xresources
+# spicetify is a little different
+ln -s ~/git-repos/dotfiles/config/spicetify/Themes/ ~/.config/spicetify/
+
+# home dir stuff
+ln -s ~/git-repos/dotfiles/Xresources        ~/.Xresources
 ln -s ~/git-repos/dotfiles/Xresources_colors ~/.Xresources_colors
-ln -s ~/git-repos/dotfiles/zshrc ~/.zshrc
-ln -s ~/git-repos/dotfiles/zprofile ~/.zprofile
+
 ln -s ~/git-repos/dotfiles/xprofile ~/.xprofile
+ln -s ~/git-repos/dotfiles/zprofile ~/.zprofile
+
+ln -s ~/git-repos/dotfiles/zshrc   ~/.zshrc
 ln -s ~/git-repos/dotfiles/xinitrc ~/.xinitrc
