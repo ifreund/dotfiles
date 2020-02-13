afb7962d7cc217ec9f5928d6d7d2c4a53369fb8e
Isaac Freund <ifreund@ifreund.xyz>
2020-02-13 18:21:00 +0100

    Clean up modal cursor setup

--- config/zsh/vi_modal_cursor.zsh
+++ config/zsh/vi_modal_cursor.zsh
@@ -1,15 +1,18 @@
 # change cursor shape depending on vi mode
 function zle-keymap-select {
-  if [[ ${KEYMAP} == vicmd ]] ||
-     [[ $1 = 'block' ]]; then
-    echo -ne '\e[1 q'
+    case $KEYMAP in
+        vicmd)
+            echo -ne '\e[1 q';;
 
-  elif [[ ${KEYMAP} == main ]] ||
-       [[ ${KEYMAP} == viins ]] ||
-       [[ ${KEYMAP} = '' ]] ||
-       [[ $1 = 'beam' ]]; then
-    echo -ne '\e[5 q'
-  fi
+        # this doesn't work currently, but maybe someday
+        # https://www.zsh.org/mla/users/2016/msg00188.html
+        viopp)
+            echo -ne '\e[4 q';;
+        
+        # main, viins, anything else
+        *)
+            echo -ne '\e[5 q';;
+    esac
 }
 zle -N zle-keymap-select
 
