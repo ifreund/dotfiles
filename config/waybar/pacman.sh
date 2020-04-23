6b2c30fb487382170d5fae68b1b4004ed6cc71e9
Isaac Freund <ifreund@ifreund.xyz>
2020-04-23 17:53:13 +0200

    Fix waybar text for exactly 1 update

--- config/waybar/pacman.sh
+++ config/waybar/pacman.sh
@@ -1,9 +1,11 @@
-#!/usr/bin/bash
+#!/bin/sh
 
 num_updates=$(checkupdates | wc -l)
 
-if [ $num_updates -gt 0 ]; then
-    echo "{\"text\": \"$num_updates updates\", \"class\": \"warning\"}" 
+if [ $num_updates -gt 1 ]; then
+    echo "{\"text\": \"$num_updates updates\", \"class\": \"warning\"}"
+elif [ $num_updates -eq 1 ]; then
+    echo "{\"text\": \"$num_updates update\", \"class\": \"warning\"}" 
 else
     echo "{\"text\": \"up to date\"}" 
 fi
