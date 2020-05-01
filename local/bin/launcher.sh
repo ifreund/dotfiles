3d62e06b1dd36b7a2e51509b7ef8526ff678bb6f
Isaac Freund <ifreund@ifreund.xyz>
2020-05-01 15:22:36 +0200

    Use dot-desktop for launcher script

--- local/bin/launcher.sh
+++ local/bin/launcher.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-bemenu-run --ignorecase --fn "cozette 10" --line-height 13 \
+target=$(dot-desktop | bemenu -i -p "exec:" --fn "cozette 10" --line-height 13 \
 	--tb=#839496 --tf=#002b36 \
 	--fb=#002b36 --ff=#839496 \
 	--nb=#073642 --nf=#93a1a1 \
-	--hb=#839496 --hf=#002b36
+	--hb=#839496 --hf=#002b36)
+
+swaymsg exec eval "$(dot-desktop "${target}")"
