80115178d4d84addbec9f7625eebc1fda78481a8
Isaac Freund <ifreund@ifreund.xyz>
2020-08-05 03:08:19 +0200

    river: always save logs

--- local/bin/start_river.sh
+++ local/bin/start_river.sh
@@ -5,4 +5,5 @@ export XKB_DEFAULT_LAYOUT="us(colemak)"
 
 export MOZ_ENABLE_WAYLAND=1
 
-exec dbus-run-session river -l 7 -c ~/.config/river/config.sh
+timestamp=$(date +%F-%R)
+exec dbus-run-session river -l 7 -c ~/.config/river/config.sh > /tmp/river-${timestamp}.log 2>&1
