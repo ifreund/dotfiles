2f14ee0d529e566832614c8c99a485c93cb701d4
Isaac Freund <ifreund@ifreund.xyz>
2020-08-05 13:03:03 +0200

    kak: use git ls-files for git repos

--- local/bin/kak_open_file.sh
+++ local/bin/kak_open_file.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 session_id=$1
 client_id=$2
-target=$(rg --files --hidden | fzy -l 32)
+if git rev-parse --is-inside-work-tree > /dev/null 2>&1
+then
+	target=$(git ls-files | fzy -l 32)
+else
+	target=$(rg --files | fzy -l 32)
+fi
 echo "eval -client ${client_id} edit -existing ${target}" | kak -p "${session_id}"
