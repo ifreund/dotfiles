cad1f20d6fce923420c248e990a58e131662da53
Isaac Freund <ifreund@ifreund.xyz>
2020-11-21 16:22:01 +0100

    Use default river config location

--- local/bin/start_river.sh
+++ local/bin/start_river.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-export XKB_DEFAULT_OPTIONS="caps:escape"
-export XKB_DEFAULT_LAYOUT="us(colemak)"
-
 export MOZ_ENABLE_WAYLAND=1
 
 timestamp=$(date +%F-%R)
-exec dbus-run-session river -l 7 -c ~/.config/river/config.sh > /tmp/river-${timestamp}.log 2>&1
+exec dbus-run-session river -l 6 > ~/river-${timestamp}.log 2>&1
