c2f16677468391f2d8b4b9d452199f27020cb8d6
Isaac Freund <ifreund@ifreund.xyz>
2021-08-04 17:07:42 +0200

    river: fix log level option

--- local/bin/start_river.sh
+++ local/bin/start_river.sh
@@ -5,4 +5,4 @@ export XDG_SESSION_TYPE=wayland
 export XDG_CURRENT_DESKTOP=river
 
 timestamp=$(date +%F-%R)
-exec dbus-run-session river -l 6 > /tmp/river-${timestamp}.log 2>&1
+exec dbus-run-session river -log-level debug > /tmp/river-${timestamp}.log 2>&1
