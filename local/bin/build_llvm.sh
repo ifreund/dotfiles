200292783c419f7b4e96b2dc1d3b0e68880d49da
Isaac Freund <mail@isaacfreund.com>
2021-11-27 12:25:55 +0100

    bin: add llvm build script

---
+++ local/bin/build_llvm.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+prefix="$HOME/.local/llvm13"
+
+# LLVM
+cd llvm
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_PREFIX_PATH=${prefix} -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_LIBXML2=OFF -G Ninja -DLLVM_PARALLEL_LINK_JOBS=1
+ninja -j 14 install
+cd ../..
+
+# LLD
+cd lld
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_PREFIX_PATH=${prefix} -DCMAKE_BUILD_TYPE=Release  -G Ninja -DLLVM_PARALLEL_LINK_JOBS=1
+ninja -j 14 install
+cd ../..
+
+# Clang
+cd clang
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_PREFIX_PATH=${prefix} -DCMAKE_BUILD_TYPE=Release  -G Ninja -DLLVM_PARALLEL_LINK_JOBS=1
+ninja -j 14 install
+cd ../..
