f7bf8176da20fe5e48f9d47a2e480c9a5f857af9
Isaac Freund <mail@isaacfreund.com>
2023-01-18 11:32:40 +0100

    build_llvm: update to llvm 15

--- local/bin/build_llvm.sh
+++ local/bin/build_llvm.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-prefix="$HOME/.local/llvm13"
+prefix="$HOME/.local/llvm15"
 
 # LLVM
 cd llvm
 mkdir build
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_PREFIX_PATH=${prefix} -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_LIBXML2=OFF -G Ninja -DLLVM_PARALLEL_LINK_JOBS=1
-ninja -j 14 install
+ninja -j 12 install
 cd ../..
 
 # LLD
@@ -15,7 +15,7 @@ cd lld
 mkdir build
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_PREFIX_PATH=${prefix} -DCMAKE_BUILD_TYPE=Release  -G Ninja -DLLVM_PARALLEL_LINK_JOBS=1
-ninja -j 14 install
+ninja -j 12 install
 cd ../..
 
 # Clang
@@ -23,5 +23,5 @@ cd clang
 mkdir build
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_PREFIX_PATH=${prefix} -DCMAKE_BUILD_TYPE=Release  -G Ninja -DLLVM_PARALLEL_LINK_JOBS=1
-ninja -j 14 install
+ninja -j 12 install
 cd ../..
