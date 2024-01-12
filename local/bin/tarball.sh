3d993383c322d5e25f872c94a11a3f86326cf08b
Isaac Freund <mail@isaacfreund.com>
2024-01-11 18:36:55 -0600

    tarball.sh: make portable
    GNU's cp has taught me bad habits, this is much more readable now as
    well.

--- local/bin/tarball.sh
+++ local/bin/tarball.sh
@@ -2,12 +2,18 @@
 
 set -e
 
-name=$1
+name="$1"
 if [ -z "$name" ]; then
 	echo "argument required: name"
 	exit 1
 fi
 
-mkdir $name
-git ls-files --recurse-submodules | xargs cp --parents -r -t $name
-tar -cavf $name.tar.gz $name
+mkdir "$name"
+for file in $(git ls-files --recurse-submodules)
+do
+	path="$name/$(dirname "$file")"
+	mkdir -p "$path"
+	cp "$file" "$path"
+done
+
+tar -cavf "$name".tar.gz "$name"
