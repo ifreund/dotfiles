4434dd203a0a2cd5e8b5c13ad0d91845adfb673a
Isaac Freund <mail@isaacfreund.com>
2024-02-11 13:27:03 +0100

    river: fix dbus env stuff

--- local/bin/start_river.sh
+++ local/bin/start_river.sh
@@ -5,4 +5,4 @@ export XDG_SESSION_TYPE=wayland
 export XDG_CURRENT_DESKTOP=river
 
 timestamp=$(date +%F-%R)
-exec dbus-run-session river -log-level debug > /tmp/river-${timestamp}.log 2>&1
+exec river -log-level debug > /tmp/river-${timestamp}.log 2>&1
