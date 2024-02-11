c814a7e240a46a0cfc656cbdfdd2cb234f6db026
Isaac Freund <mail@isaacfreund.com>
2024-02-11 13:35:05 +0100

    river: no real need for debug logs

--- local/bin/start_river.sh
+++ local/bin/start_river.sh
@@ -5,4 +5,4 @@ export XDG_SESSION_TYPE=wayland
 export XDG_CURRENT_DESKTOP=river
 
 timestamp=$(date +%F-%R)
-exec river -log-level debug > /tmp/river-${timestamp}.log 2>&1
+exec river -log-level warning > /tmp/river-${timestamp}.log 2>&1
