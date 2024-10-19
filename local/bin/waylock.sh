36b335096f952d66f49172dce3849d33bfe10606
Isaac Freund <mail@isaacfreund.com>
2024-10-19 16:46:43 +0200

    waylock.sh: don't set WAYLAND_DEBUG for chayang

--- local/bin/waylock.sh
+++ local/bin/waylock.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 timestamp=$(date +%F-%R:%S)
+
+chayang || exit 1
+
 export WAYLAND_DEBUG=1
-chayang && waylock -fork-on-lock > /tmp/waylock-${timestamp}.log 2>&1
+waylock -fork-on-lock > /tmp/waylock-${timestamp}.log 2>&1
