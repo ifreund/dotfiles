3017fc5c490790ad9d6ab3c4cba64412eadad145
Isaac Freund <mail@isaacfreund.com>
2025-12-02 11:06:42 +0100

    darkman: more robust rijan support

--- local/share/darkman/rijan.sh
+++ local/share/darkman/rijan.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 case "$1" in
-dark|light) echo "(do (merge-into config ${1}) (:manage-dirty ((wm :registry) :rwm)))" | janet-netrepl -c ;;
+dark|light)
+	for path in $(find "${XDG_RUNTIME_DIR}" -name "rijan-*"); do
+		echo "(do (merge-into config ${1}) (:manage-dirty ((wm :registry) :rwm)))" | \
+			janet-netrepl -U "${path}" -c
+	done
+	;;
 default) exit 1 ;;
 esac
