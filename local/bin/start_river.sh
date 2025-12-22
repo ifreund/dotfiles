24018a3dc5cdbbbf28967d75eab5ce19595ed012
Isaac Freund <mail@isaacfreund.com>
2025-12-22 16:06:07 +0000

    river: configure xkb with env var
    TODO implement xkb config in river 0.4.0-dev

--- local/bin/start_river.sh
+++ local/bin/start_river.sh
@@ -2,6 +2,7 @@
 
 export XDG_SESSION_TYPE=wayland
 export XDG_CURRENT_DESKTOP=river
+export XKB_DEFAULT_OPTIONS=compose:ralt
 
 timestamp=$(date +%F-%R)
 exec river -log-level debug > /tmp/river-${timestamp}.log 2>&1
