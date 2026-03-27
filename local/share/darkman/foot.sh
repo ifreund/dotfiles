02fb1850dd5f096941ccc4c7b6e0f7537c085a0a
Isaac Freund <mail@isaacfreund.com>
2026-03-27 19:33:08 +0100

    foot: fix deprecated config

--- local/share/darkman/foot.sh
+++ local/share/darkman/foot.sh
@@ -2,10 +2,10 @@
 
 case "$1" in
 dark)
-	echo "initial-color-theme=1" > ~/.config/foot/colors.ini
+	echo "initial-color-theme=dark" > ~/.config/foot/colors.ini
 	killall -s SIGUSR1 foot ;;
 light)
-	echo "initial-color-theme=2" > ~/.config/foot/colors.ini
+	echo "initial-color-theme=light" > ~/.config/foot/colors.ini
 	killall -s SIGUSR2 foot ;;
 default) exit 1 ;;
 esac
